0e1e2f900d48d276cc0a10633c46cace337deabc
taiga905 <mochidukiyukimi@gmail.com>
2025-02-04 19:09:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -291,10 +291,10 @@ def view_commonds(request: Request,yuki: Union[str] = Cookie(None)):
     return how_cached()
 
 @app.get("/verify", response_class=HTMLResponse)
-def get_form(seed):
+def get_form(seed=""):
     return requests.get(fr"{url}verify?seed={urllib.parse.quote(seed)}").text
 
-@app.post("/submit")
+@app.post("/submit", response_class=HTMLResponse)
 def submit(h_captcha_response: str = Form(alias="h-captcha-response"), seed: str = Form(...)):
     return requests.post(fr"{url}submit",data={"h-captcha-response": h_captcha_response, "seed": seed}).text
 
